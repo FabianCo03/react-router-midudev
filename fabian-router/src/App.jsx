c06ce59f2c167fea9ff25061dde781d1a5330637
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-07-19 11:20:00 -0500

    feat: 🎸 Creando una SPA

--- fabian-router/src/App.jsx
+++ fabian-router/src/App.jsx
@@ -1,42 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function HomePage() {
-  return (
-    <>
-    <h1>Estás en Home</h1>
-    <p>Página de ejemplo para react router</p>
-    <a href="/about">Sobre mí</a>
-    </>
-  )
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  // evento personalizado
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
 }
 
-function AboutPage() {
+function HomePage() {
+  return (
+    <>
+      <h1>Estás en Home</h1>
+      <p>Página de ejemplo para react router</p>
+      <button onClick={() => navigate('/about')} href="/about">Sobre mí</button>
+    </>
+  );
+}
+
+function AboutPage() {
   return (
     <>
       <h1>About</h1>
       <p>Hola! me llamo Fabián y estoy creando mi primer React Router</p>
       <div>
-        <img src="https://pbs.twimg.com/profile_images/1424599144627380226/Ty7fEdbn_400x400.jpg" alt="Foto perfil Twitter FabianCo03" />
+        <img
+          src="https://pbs.twimg.com/profile_images/1424599144627380226/Ty7fEdbn_400x400.jpg"
+          alt="Foto perfil Twitter FabianCo03"
+        />
       </div>
-      <a href="/">Ir al Home</a>
+      <button onClick={() => navigate('/')} href="/">Ir al Home</button>
     </>
-  )
+  );
 }
 
 function App() {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+
+  }, [])
 
   return (
     <>
       <main>
-        {currentPath === '/' && <HomePage />}
-        {currentPath === '/about' && <AboutPage />}
+        {currentPath === "/" && <HomePage />}
+        {currentPath === "/about" && <AboutPage />}
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
