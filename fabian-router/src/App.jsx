7f34f4390a2362205ba39e0118efd55f689e143c
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-07-21 12:19:27 -0500

    feat: 🎸 Rutas 404 y rutas por defecto

--- fabian-router/src/App.jsx
+++ fabian-router/src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
 import { Router } from "./Router";
+import Page404 from "./pages/404";
 
 const appRoutes = [
   {
@@ -22,7 +23,7 @@ function App() {
   return (
     <>
       <main>
-        <Router routes={appRoutes}></Router>
+        <Router routes={appRoutes} defaultComponent={Page404}></Router>
       </main>
     </>
   );
